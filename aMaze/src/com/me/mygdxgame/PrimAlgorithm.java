092783544764028428f6333aca35a753946133b2
Shean Kim <sheankim@Sheans-MacBook-Air.local>
2013-04-18 23:28:50 -0400

    Finalized the algorithm, still need to add in the drawing part:

--- aMaze/src/com/me/mygdxgame/PrimAlgorithm.java
+++ aMaze/src/com/me/mygdxgame/PrimAlgorithm.java
@@ -29,6 +29,44 @@ public class PrimAlgorithm extends Algorithm {
 		}
 		Cell startPosition = new Cell(startX, startY);
 		currentCell = startPosition;
+		
+		states[currentCell.xcoordinate][currentCell.ycoordinate] = IN;
+		for(int i = 0; i < 4; i++){
+			switch(i){
+			case UP:
+				if(currentCell.ycoordinate > 0){
+					Cell frontierCell = new Cell(currentCell.xcoordinate, currentCell.ycoordinate-1);
+					states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+					frontierCells.add(frontierCell);
+					frontierCount++;
+				}
+				break;
+			case RIGHT:
+				if(currentCell.xcoordinate < width -1){
+					Cell frontierCell = new Cell(currentCell.xcoordinate+1, currentCell.ycoordinate);
+					states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+					frontierCells.add(frontierCell);
+					frontierCount++;
+				}
+				break;
+			case DOWN:
+				if(currentCell.ycoordinate < height-1){
+					Cell frontierCell = new Cell(currentCell.xcoordinate, currentCell.ycoordinate+1);
+					states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+					frontierCells.add(frontierCell);
+					frontierCount++;
+				}
+				break;
+			case LEFT:
+				if(currentCell.xcoordinate > 0){
+					Cell frontierCell = new Cell(currentCell.xcoordinate-1, currentCell.ycoordinate);
+					states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+					frontierCells.add(frontierCell);
+					frontierCount++;
+				}
+				break;
+			}
+		}
 	}
 	
 	public PrimAlgorithm(int[][] matrix, int x, int y){
@@ -121,8 +159,48 @@ public class PrimAlgorithm extends Algorithm {
 			carve(inNeighbors[rand.nextInt(inNeighborCount)]);
 		
 			//Mark this cell as IN
+			states[frontier.xcoordinate][frontier.ycoordinate] = IN;
+			frontierCells.remove(frontierIndex);
+			frontierCount--;
 			
+			//Mark any neighboring OUT cells as FRONTIER
 			
+			for(int i = 0; i < 4; i++){
+				switch(i){
+				case UP:
+					if(frontier.ycoordinate > 0 && states[frontier.xcoordinate][frontier.ycoordinate-1] == OUT){
+						Cell frontierCell = new Cell(frontier.xcoordinate, frontier.ycoordinate-1);
+						states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+						frontierCells.add(frontierCell);
+						frontierCount++;
+					}
+					break;
+				case RIGHT:
+					if(frontier.xcoordinate < width-1 && states[frontier.xcoordinate+1][frontier.ycoordinate] == OUT){
+						Cell frontierCell = new Cell(frontier.xcoordinate+1, frontier.ycoordinate);
+						states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+						frontierCells.add(frontierCell);
+						frontierCount++;
+					}
+					break;
+				case DOWN:
+					if(frontier.ycoordinate < height-1 && states[frontier.xcoordinate][frontier.ycoordinate+1] == OUT){
+						Cell frontierCell = new Cell(frontier.xcoordinate, frontier.ycoordinate+1);
+						states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+						frontierCells.add(frontierCell);
+						frontierCount++;
+					}
+					break;
+				case LEFT:
+					if(frontier.xcoordinate > 0 && states[frontier.xcoordinate-1][frontier.ycoordinate] == OUT){
+						Cell frontierCell = new Cell(frontier.xcoordinate-1, frontier.ycoordinate);
+						states[frontierCell.xcoordinate][frontierCell.ycoordinate] = FRONTIER;
+						frontierCells.add(frontierCell);
+						frontierCount++;
+					}
+					break;
+				}
+			}
 			return true;
 		}
 		else{
